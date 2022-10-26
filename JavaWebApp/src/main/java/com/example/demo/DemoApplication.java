4800e5882466e2e8a194b577b1bac4d9327c0222
christelle ngnintedem <christellechristy485@gmail.com>
2022-10-25 21:30:45 -0400

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,15 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		string name = "christy";
+		String name = "vamsi";
 
-		system.out.println(name);
-<<<<<<< HEAD
-
-		Integer age = "20";
-		system.out.println(age + 5);
-=======
->>>>>>> 73ec79bf721f677e15eb56326138a22b14466cad
+		System.out.println(name);
 	}
 
 }
